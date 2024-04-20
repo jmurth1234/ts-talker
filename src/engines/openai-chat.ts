ce97c8410da7ec80de1a8e0f93f3f62c649e18f1
rymate1234 <ryan.murthick@gmail.com>
2024-04-20 18:39:53 +0100

    chore: remove emojis

--- src/engines/openai-chat.ts
+++ src/engines/openai-chat.ts
@@ -52,7 +52,7 @@ export const describeImage = memoize(
             {
               type: "text",
               text:
-                "Describe the image as succinctly as possible. When doing so, compress the text in a way that fits in a tweet (ideally). This is for yourself. It does not need to be human readable or understandable. Ensure the whole image is described. Abuse of language mixing, abbreviations, symbols (unicode and emoji), or any other encodings or internal representations is all permissible, as long as it, if pasted in a new inference cycle, will yield near-identical results as the original image.",
+                "Describe the image as succinctly as possible. When doing so, compress the text in a way that fits in a tweet (ideally). This is for yourself. It does not need to be human readable or understandable. Ensure the whole image is described. Abuse of language mixing, abbreviations, symbols, or any other encodings or internal representations is all permissible, as long as it, if pasted in a new inference cycle, will yield near-identical results as the original image.",
             },
           ],
         },
@@ -71,7 +71,7 @@ export const describeImage = memoize(
 export const describeEmbed = memoize(
   async (text: string, model = "claude-3-haiku-20240307") => {
     const description = await Anthropic.getInstance().messages.create({
-      system: "Describe the following embed. When doing so, compress the text in a way that fits in a tweet (ideally) and such that you or another language model can reconstruct the intention of the human who wrote text as close as possible to the original intention. This is for yourself. It does not need to be human readable or understandable. Abuse of language mixing, abbreviations, symbols (unicode and emoji), or any other encodings or internal representations is all permissible, as long as it, if pasted in a new inference cycle, will yield near-identical results as the original embed",
+      system: "Describe the following embed. When doing so, compress the text in a way that fits in a tweet (ideally) and such that you or another language model can reconstruct the intention of the human who wrote text as close as possible to the original intention. This is for yourself. It does not need to be human readable or understandable. Abuse of language mixing, abbreviations, symbols, or any other encodings or internal representations is all permissible, as long as it, if pasted in a new inference cycle, will yield near-identical results as the original embed",
       messages: [
         {
           role: "user",
