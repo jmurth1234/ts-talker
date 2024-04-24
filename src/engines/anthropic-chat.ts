7c43236ab35788106d4d699dc995598b060f120a
rymate1234 <ryan.murthick@gmail.com>
2024-04-24 20:18:50 +0100

    argh

--- src/engines/anthropic-chat.ts
+++ src/engines/anthropic-chat.ts
@@ -412,7 +412,7 @@ class AnthropicChatEngine extends TextEngine {
     console.log("tools", tools);
     console.dir(chatMessages, { depth: 3 });
 
-    const response = await Anthropic.getInstance().beta.tools.messages.create({
+    const response = await Anthropic.getInstance().messages.create({
       system,
       messages: [
         ...chatMessages,
